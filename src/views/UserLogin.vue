f1b7c150b5a80c99e2a6ff2b131091eeb2b9ab9a
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-11-26 15:05:13 -0300

    added title

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="home">
-        <h4 id="brand-login">People Contacts</h4>
+        <h4 id="brand-login">People</h4>
         <p id="words-login">All your contacts by domain</p>  
         <p>Please, come and share your contacts!</p>
         <button 
